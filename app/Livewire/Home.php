e79f287e8bf8dbea3b8bf5f35d57d251609ca366
Rajesh Budhathoki <rajoyish@gmail.com>
2024-05-21 09:39:59 +0545

    Creating employees and services tables

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\Employee;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
+        dd(Employee::find(2)->services);
+
         return view('livewire.home');
     }
 }
