8a0936285e0883226a58e184280025132debbe3d
Rajesh Budhathoki <rajoyish@gmail.com>
2024-05-22 07:53:46 +0545

    Creating the appointments table

---
+++ app/Models/Appointment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'starts_at' => 'datetime',
+        'ends_at' => 'datetime',
+    ];
+
+    public static function booted()
+    {
+        static::creating(function (Appointment $appointment) {
+            $appointment->uuid = str()->uuid();
+        });
+    }
+
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
