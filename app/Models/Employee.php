db5fb758eda9c34ea2ec30f257070f0bc407f5ae
Rajesh Budhathoki <rajoyish@gmail.com>
2024-05-22 07:33:57 +0545

    Employee schedules and exclusions tables

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employee extends Model
 {
@@ -13,5 +14,15 @@ class Employee extends Model
     public function services(): BelongsToMany
     {
         return $this->belongsToMany(Service::class);
+    }
+
+    public function schedules(): HasMany
+    {
+        return $this->hasMany(Schedule::class);
+    }
+
+    public function scheduleExclusions(): HasMany
+    {
+        return $this->hasMany(ScheduleExclusion::class);
     }
 }
