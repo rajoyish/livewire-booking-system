f5664539c554a45e310d30c95cca6d2c6670d529
Rajesh Budhathoki <rajoyish@gmail.com>
2024-05-23 08:35:29 +0545

    Displaying employees and services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cknow\Money\Casts\MoneyIntegerCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,6 +10,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Service extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'price' => MoneyIntegerCast::class,
+    ];
 
     public function employees(): BelongsToMany
     {
