b72bc8e5556f7315ee385f575fb2cb20daab07c1
Rajesh Budhathoki <rajoyish@gmail.com>
2024-05-28 09:42:38 +0545

    Adding schedule availability

--- app/Booking/ScheduleAvailability.php
+++ app/Booking/ScheduleAvailability.php
@@ -2,15 +2,19 @@
 
 namespace App\Booking;
 
+use App\Models\Employee;
+use App\Models\Service;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
+use Spatie\Period\Period;
 use Spatie\Period\PeriodCollection;
+use Spatie\Period\Precision;
 
 class ScheduleAvailability
 {
     protected PeriodCollection $periods;
 
-    public function __construct()
+    public function __construct(protected Employee $employee, protected Service $service)
     {
         $this->periods = new PeriodCollection();
     }
@@ -19,9 +23,29 @@ class ScheduleAvailability
     {
         collect(CarbonPeriod::create($startsAt, $endsAt)->days())
             ->each(function (Carbon $date) {
-                dump($date->format('l'));
+                $this->addAvailabilityFromSchedule($date);
             });
 
         return $this->periods;
+    }
+
+    public function addAvailabilityFromSchedule(Carbon $date)
+    {
+        if (! $schedule = $this->employee->schedules()->where('starts_at', '<=', $date)->where('ends_at', '>=', $date)->first()) {
+            return;
+        }
+
+        if (! [$startsAt, $endsAt] = $schedule->getWorkingHoursForDate($date)) {
+            return;
+        }
+
+        $this->periods = $this->periods->add(
+            Period::make(
+                $date->copy()->setTimeFromTimeString($startsAt),
+                $date->copy()->setTimeFromTimeString($endsAt)->subMinutes($this->service->duration),
+                Precision::MINUTE()
+            )
+        );
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,21 @@
 use App\Booking\ScheduleAvailability;
 use App\Livewire\EmployeeShow;
 use App\Livewire\Home;
+use App\Models\Employee;
+use App\Models\Service;
 use Illuminate\Support\Facades\Route;
+
+//Carbon::setTestNow(now()->addDay());
 
 Route::get('/', Home::class)->name('home');
 
 Route::get('/employees/{employee:slug}', EmployeeShow::class)->name('employees.show');
 
 Route::get('/periods', function () {
-    $availability = (new ScheduleAvailability())
+    $employee = Employee::find(1);
+    $service = Service::find(1);
+
+    $availability = (new ScheduleAvailability($employee, $service))
         ->forPeriod(now()->startOfDay(), now()->addDay()->endOfDay());
 
     dd($availability);
