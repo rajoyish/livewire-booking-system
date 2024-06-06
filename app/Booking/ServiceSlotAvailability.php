e03534b15c7f1f8a76850e15e03d9b0cb23bf20e
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-06 08:49:07 +0545

    Adding employees into slots

--- app/Booking/ServiceSlotAvailability.php
+++ app/Booking/ServiceSlotAvailability.php
@@ -6,6 +6,7 @@ use App\Models\Employee;
 use App\Models\Service;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use Spatie\Period\Period;
 
 class ServiceSlotAvailability
 {
@@ -17,8 +18,26 @@ class ServiceSlotAvailability
     {
         $range = (new SlotGenerator($startsAt, $endsAt))->generate($this->service->duration);
 
-        $this->employees->each(function (Employee $employee) {
+        $this->employees->each(function (Employee $employee) use ($startsAt, $endsAt, &$range) {
+            $periods = (new ScheduleAvailability($employee, $this->service))->forPeriod($startsAt, $endsAt);
 
+            foreach ($periods as $period) {
+                $this->addAvailableEmployeeForPeriod($range, $period, $employee);
+            }
+
+            dd($range);
+
+        });
+    }
+
+    public function addAvailableEmployeeForPeriod(DateCollection $range, Period $period, Employee $employee)
+    {
+        $range->each(function (Date $date) use ($period, $employee) {
+            $date->slots->each(function (Slot $slot) use ($period, $employee) {
+                if ($period->contains($slot->time)) {
+                    $slot->addEmployee($employee);
+                }
+            });
         });
     }
 }
