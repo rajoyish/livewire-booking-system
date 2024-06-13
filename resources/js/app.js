dc8bc5348af6053273a9ae56d08e205c0b7b8101
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-13 09:14:32 +0545

    Building Alpine plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import "./bootstrap";
+import {
+    Alpine,
+    Livewire,
+} from "../../vendor/livewire/livewire/dist/livewire.esm";
+import picker from "./picker.js";
+
+Alpine.plugin(picker);
+
+Livewire.start();
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,5 +11,6 @@
 <div class="max-w-4xl mx-auto my-12 px-6">
     {{ $slot }}
 </div>
+@livewireScriptConfig
 </body>
 </html>
